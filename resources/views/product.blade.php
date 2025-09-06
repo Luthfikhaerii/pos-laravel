9035cfab873f2e784aec3f33dcb2149e9c0d777d
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-09-06 16:30:34 +0700

    update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,12 +7,12 @@
     <title>Dashboard</title>
    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body >
-    <div class="flex min-h-screen h-full">
+<body class="min-h-screen">
+    <div class="flex min-h-screen">
         @include('components.sidebar')
-        <section class="flex-1 flex flex-col h-full">
+        <section class="flex-1 flex flex-col min-h-screen h-full">
            <div class="h-20 bg-gray-200 w-full">
-                <p class="text-2xl">Product List</p>
+                <p class="ml-2 text-2xl">Product List</p>
            </div>
            <div class="flex w-full px-4">
                 <div class="w-36 h-14 flex items-center border-b-4 border-[#4C81F1]">
@@ -25,8 +25,8 @@
                     <p class="text-center font-semibold m-auto">Drink</p>
                 </div>
            </div>
-           <div  class="bg-gray-100 w-full min-h-[100%]">
-               kosong
+           <div  class="bg-gray-100 w-full flex-1">
+               <p>woyy</p>
            </div>
         </section>
     </div>
